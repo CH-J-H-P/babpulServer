2dcdd60d0beb654bac7fbb193c5f6a8992f05178
CH-J-H-P <1004skfro7@naver.com>
2025-06-28 10:28:14 +0900

    받아라잇

--- src/main/java/com/example/babpulServer/config/CorsConfig.java
+++ src/main/java/com/example/babpulServer/config/CorsConfig.java
@@ -5,17 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration// 스프링의 설정 클래스임
+@Configuration
 public class CorsConfig {
-    @Bean // 메서드의 반환 객체를 Spring IoC 컨테이너에 bean 으로 등록
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
-                        .allowedOrigins("https://babpul.pages.dev", "http://localhost:5173", "http://127.0.0.1:8080") // 클라이언트 도메인 허용
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                        .allowCredentials(true); // 쿠키 및 인증 정보를 포함한 요청 허용
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // 모든 origin 허용
+                        .allowedMethods("*") // 모든 HTTP 메서드 허용
+                        .allowedHeaders("*"); // 모든 헤더 허용
             }
         };
     }
